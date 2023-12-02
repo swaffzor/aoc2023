f76bedde8484db033b611e16a34ae193c7c0a8f6
Jeremy Swafford <jeremy@swafford.co>
2023-12-02 14:57:13 -0500

    day 2 vis: add controls

--- visualizations/day2-vis/src/App.tsx
+++ visualizations/day2-vis/src/App.tsx
@@ -6,13 +6,22 @@ import input from './input.json'
 
 const GRID_SIZE = 3
 function App() {
+  const [inputMode, setInputMode] = React.useState<'sample' | 'puzzle'>(
+    'puzzle'
+  )
+  const [renderMode, setRenderMode] = React.useState<'static' | 'animate'>(
+    'static'
+  )
   const [data, setData] = React.useState<Roll[][]>([])
   const [timeTick, setTimeTick] = React.useState(0)
-  const gameData = extractData(JSON.stringify(input))
+  const gameData = extractData(
+    JSON.stringify(inputMode === 'puzzle' ? input : sampleInput)
+  )
   const games = gameData[0].rolls
+  const dataSource = renderMode === 'static' ? games : data
 
   React.useEffect(() => {
-    timeTick < games.length && setData([games[timeTick]])
+    timeTick < games.length ? setData([games[timeTick]]) : setTimeTick(0)
   }, [timeTick])
 
   React.useEffect(() => {
@@ -23,11 +32,34 @@ function App() {
     return () => clearInterval(interval)
   }, [timeTick])
 
+  React.useEffect(() => {
+    setTimeTick(0)
+  }, [renderMode, inputMode])
+
+  const buttonStyles =
+    'px-2 m-4 text-gray-400 border-2 border-gray-400 rounded-md'
+
   return (
     <>
       <h1 className="text-3xl">AoC 2023 day 2</h1>
+      <button
+        className={buttonStyles}
+        onClick={() =>
+          setInputMode((mode) => (mode === 'puzzle' ? 'sample' : 'puzzle'))
+        }
+      >
+        use {inputMode === 'puzzle' ? 'sample' : 'puzzle'} data
+      </button>
+      <button
+        className={buttonStyles}
+        onClick={() =>
+          setRenderMode((mode) => (mode === 'static' ? 'animate' : 'static'))
+        }
+      >
+        set mode to {renderMode === 'static' ? 'animate' : 'static'}
+      </button>
       <div className={`grid grid-cols-${GRID_SIZE}`}>
-        {games.map((game, index) => {
+        {dataSource.map((game, index) => {
           return (
             <div
               key={`game-row-${index}`}
@@ -87,7 +119,7 @@ const PullCell = ({ color, count }: { color: string; count: number }) => {
         return (
           <div
             key={`data-${i}`}
-            className={`${sizeStyles} bg-${color}-500`}
+            className={`${sizeStyles} bg-${color}-500 transition duration-500 ease-in-out `}
           ></div>
         )
       })}
