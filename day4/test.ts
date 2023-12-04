6f41cf01b60c956ae1eeda1d985632a60f7a571c
Jeremy Swafford <jeremy@swafford.co>
2023-12-04 12:57:50 -0500

    day4 part2 complete

--- day4/test.ts
+++ day4/test.ts
@@ -1,9 +1,13 @@
 import { getPuzzleInput } from '../utils'
-import { part1 } from './solution'
+import { part1, part2 } from './solution'
 
 describe('day 4', () => {
-  it('part 1', () => {
-    expect(part1(getPuzzleInput('day4/sampleInput'))).toBe(13)
+  it.skip('part 1', () => {
+    // expect(part1(getPuzzleInput('day4/sampleInput'))).toBe(13)
     console.log('part1', part1(getPuzzleInput('day4/input')))
+  })
+  it('part 2', () => {
+    // expect(part2(getPuzzleInput('day4/sampleInput'))).toBe(30)
+    console.log('part2', part2(getPuzzleInput('day4/input')))
   })
 })
