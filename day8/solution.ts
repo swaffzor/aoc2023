2c58d1305a347f213bbae3d2b3891a1411e5ff79
Jeremy Swafford <jeremy@swafford.co>
2023-12-08 08:14:17 -0500

    day8 part2

--- day8/solution.ts
+++ day8/solution.ts
@@ -71,3 +71,73 @@ export const part1 = (input: string): number => {
   }
   return steps
 }
+
+/*
+--- Part Two ---
+After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z.
+
+For example:
+
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+Step 0: You are at 11A and 22A.
+Step 1: You choose all of the left paths, leading you to 11B and 22B.
+Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+Step 5: You choose all of the left paths, leading you to 11B and 22C.
+Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+*/
+
+export const part2 = (input: string) => {
+  const data = extractData(input)
+  const directions = data.directions.split('')
+  const start = data.map.filter((line) => line.name[2] === 'A') || []
+
+  const temp = start.map((track) => {
+    let steps = 0
+    let index = 0
+    let currentNode = track
+    while (currentNode.name[2] !== 'Z') {
+      const direction = directions[index++ % directions.length]
+
+      const newNode = data.map.find((line) =>
+        direction === 'L'
+          ? line.name === currentNode.L
+          : line.name === currentNode.R
+      ) || {
+        name: '',
+        L: '',
+        R: '',
+      }
+      currentNode = newNode
+      steps++
+    }
+    return steps
+  })
+  console.log(temp)
+  return temp.reduce(lcm)
+  // return temp.reduce((acc, val) => acc * val, 1)
+  // 22103062509257 is correct (22,103,062,509,257) (taken the logged values of temp and input into a lowest common multiple calculator)
+}
+
+function lcm(a: number, b: number): number {
+  let temp = a
+  while (true) {
+    if (temp % b === 0 && temp % a === 0) return temp
+    temp++
+  }
+}
--- day8/test.ts
+++ day8/test.ts
@@ -1,5 +1,5 @@
 import { getPuzzleInput } from '../utils'
-import { part1 } from './solution'
+import { part1, part2 } from './solution'
 
 describe('day8', () => {
   describe('part1', () => {
@@ -21,9 +21,22 @@ ZZZ = (ZZZ, ZZZ)`
 
       expect(part1(input1)).toBe(2)
       expect(part1(input2)).toBe(6)
-      console.log('----------------------------------------')
-      console.log('part1:', part1(getPuzzleInput('day8/input')))
-      console.log('----------------------------------------')
+    })
+  })
+  describe('part2', () => {
+    it.only('part2 ', () => {
+      const input = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+      expect(part2(input)).toBe(6)
+      console.log(part2(getPuzzleInput('day8/input')))
     })
   })
 })
