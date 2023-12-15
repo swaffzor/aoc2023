0c8e8a9bbd00c6b7430d61fa8d27eeb5252e1763
Jeremy Swafford <jeremy@swafford.co>
2023-12-15 11:46:04 -0500

    day15 part2

--- day15/test.ts
+++ day15/test.ts
@@ -1,5 +1,5 @@
 import { getPuzzleInput } from '../utils'
-import { part1 } from './solution'
+import { part1, part2 } from './solution'
 
 describe('day 15', () => {
   it('part1', () => {
@@ -8,5 +8,12 @@ describe('day 15', () => {
       1320
     )
     console.log('part 1:', part1(getPuzzleInput('day15/input')))
+  })
+
+  it.only('part2', () => {
+    expect(part2('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7')).toBe(
+      145
+    )
+    console.log('part 2:', part2(getPuzzleInput('day15/input')))
   })
 })
