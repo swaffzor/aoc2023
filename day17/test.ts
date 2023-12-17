d13e06266b551fe8f0b1f5672f54a689a40ca790
Jeremy Swafford <jeremy@swafford.co>
2023-12-17 08:02:24 -0500

    day17 ready

---
+++ day17/test.ts
@@ -0,0 +1,11 @@
+import { getPuzzleInput } from 'utils'
+import { part1 } from './solution'
+
+describe('day 17', () => {
+  describe('part 1', () => {
+    it('example', () => {
+      expect(part1(getPuzzleInput('day17/sampleInput'))).toBe(102)
+      console.log('part 1:', part1(getPuzzleInput('day17/input')))
+    })
+  })
+})
