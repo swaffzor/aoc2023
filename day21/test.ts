19f21dc52252010b7b033525b934d4faaf2ac83f
Jeremy Swafford <jeremy@swafford.co>
2023-12-23 12:09:43 -0500

    day21 part2 setup

--- day21/test.ts
+++ day21/test.ts
@@ -1,12 +1,23 @@
 import { getPuzzleInput } from '../utils'
-import { part1 } from './solution'
+import { part1, part2 } from './solution'
 
 describe('day 21', () => {
   describe('part 1', () => {
     it('solution', () => {
       const input = getPuzzleInput('day21/sampleInput')
       expect(part1(input, 6)).toBe(16)
-      console.log('part 1:', part1(getPuzzleInput('day21/input'), 64))
+      // console.log('part 1:', part1(getPuzzleInput('day21/input'), 64))
+    })
+  })
+  describe('part 2', () => {
+    it.each([
+      [6, 16],
+      [10, 50],
+      [50, 1594],
+    ])('%s steps', (steps, expected) => {
+      const input = getPuzzleInput('day21/sampleInput2')
+      expect(part2(input, steps)).toBe(expected)
+      // console.log('part 2:', part2(getPuzzleInput('day21/input'), Infinity))
     })
   })
 })
